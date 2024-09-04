d9639f4c840b01ec85b11144608a3f751ccc79d3
kirtan1313 <mkirtan013@gmail.com>
2024-09-04 20:36:30 +0530

    update2

--- Confring/dataBase.js
+++ Confring/dataBase.js
@@ -1,8 +1,7 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb+srv://mkirtan013:<db_password>@cluster0.egcsg.mongodb.net/Expense-tracker')
-  .then(() => console.log('Data base is Connected Successfully....')).catch((err)=>{
-    console.log('Database conectivity is faild..!',err);
+mongoose.connect('mongodb+srv://mkirtan013:KvJNO0YeJrikMEFt@cluster0.egcsg.mongodb.net/Expense-tracker')
+  .then(() => console.log('Data base is Connected Successfully....')).catch((err) => {
+    console.log('Database conectivity is faild..!', err);
   });
 
-  
